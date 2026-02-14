3f86263b87d6d361710a36fbc485ef91851a2ac8
AvishMD <avishmd2007@gmail.com>
2026-02-14 09:46:20 +0530

    Update p12.c

--- p12.c
+++ p12.c
@@ -3,7 +3,7 @@ int main() {
     int a,b,result=1;
     scanf("%d %d",&a,&b);
     for(int i=1;i<=b;i++ ){
-         result=result*i;
+         result=result*a;
     }
     printf("%d", result);
     return 0;
